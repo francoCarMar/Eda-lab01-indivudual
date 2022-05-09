d6dea975bddef9a55162e98ba7b2ec2fde8476a2
franco <franco@debian.fluchi>
2022-05-09 11:37:07 -0500

    agrego mi nombre

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Bienvenida al curso");
+        System.out.println("FRANCO LUCHIANO CARDENAS MARTIENEZ");
     }
 
 }
